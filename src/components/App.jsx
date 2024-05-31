31ed7350462633f82ac610554b2f14826d4ece15
Ankush Kumar <imankush1010@gmail.com>
2024-05-31 06:38:19 +0530

    Setting goals in readme

---
+++ src/components/App.jsx
@@ -0,0 +1,35 @@
+import FriendList from "./FriendList";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+const App = () => {
+  const friends = initialFriends;
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList friends={friends} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/FriendList.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Button from "./Button";
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <li>
+          <Friend friend={friend} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+function Friend({ friend }) {
+  return (
+    <>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {friend.balance}
+        </p>
+      )}
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <Button>Select</Button>
+    </>
+  );
+}
+
+export default FriendList;
